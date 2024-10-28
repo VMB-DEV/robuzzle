c9ef6d79ffe1981683c1071188fceb11e40d4b62
VMB-DEV <vkdev7360@gmail.com>
2024-10-28 21:58:19 +0100

    feat: animate the winning widget

--- lib/features/level/presentation/page/map/win/widget_win.dart
+++ lib/features/level/presentation/page/map/win/widget_win.dart
@@ -4,8 +4,6 @@ import 'package:robuzzle/core/extensions/constraints.dart';
 import 'package:robuzzle/core/widgets/BlurryContainer.dart';
 import 'package:robuzzle/features/level/presentation/page/map/win/painter_winner_star.dart';
 
-import '../../../../../../core/log/consolColors.dart';
-
 class WinWidget extends StatefulWidget {
   const WinWidget({super.key});
 
@@ -13,81 +11,111 @@ class WinWidget extends StatefulWidget {
   State<WinWidget> createState() => _WinWidgetState();
 }
 
-class _WinWidgetState extends State<WinWidget> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
+class _WinWidgetState extends State<WinWidget> with TickerProviderStateMixin {
+  late AnimationController _loopController;
+  late AnimationController _displayController;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
+    _loopController = AnimationController(
+      lowerBound: 0.7,
+      upperBound: 1.0,
+      duration: const Duration(seconds: 3),
       vsync: this,
     )..repeat(reverse: true);
-    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+    _displayController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    )..forward();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _loopController.dispose();
+    _displayController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      builder: (context, box) {
-        return AnimatedBuilder(
-          animation: _animation,
-          builder: (context, child) {
-            return CustomPaint(
-              painter: WinnerStarPainter(_animation.value),
-              child: BlurryContainer(
-                height: box.H,
-                width: box.W,
-                alignment: Alignment.center,
-                borderRadius: BorderRadius.zero,
-                blurSigmaX: 4,
-                blurSigmaY: 4,
-                child:  Container(
-                  height: box.H,
-                  width: box.W,
-                  alignment: Alignment.center,
-                  child: BorderedText(
-                    strokeWidth: 2.5,
-                    strokeJoin: StrokeJoin.round,
-                    strokeColor: Colors.yellow.shade700,
-                    child: Text(
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        shadows: [
-                          const Shadow(
-                            offset: Offset(-0.5, -0.5),
-                            blurRadius: 3.5,
-                            color: Colors.black,
-                          ),
-                          Shadow(
-                            offset: const Offset(1.2, 1.2),
-                            blurRadius: 5.0,
-                            color: Colors.yellow.shade700,
-                          ),
-                        ],
-                      ),
-                      "WIN",
-                    ),
-                  ),
-                ),
-              ),
-            );
-          },
-            // );
-            // return CustomPaint(
-            //     painter: WinnerStarPainter(_animation.value),
-            //     child: const Text("WIN")
-            //   );
-          // },
+        builder: (context, box) {
+          return Stack(
+            children: [
+              _starBackground(box),
+              _foreground(box),
+            ],
+          );
+        }
+    );
+  }
+
+  Widget _winText(BoxConstraints box) {
+    return BorderedText(
+      strokeWidth: 2.5,
+      strokeJoin: StrokeJoin.round,
+      strokeColor: Colors.yellow.shade700,
+      child: Text(
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+          shadows: [
+            const Shadow(
+              offset: Offset(-0.5, -0.5),
+              blurRadius: 3.5,
+              color: Colors.black,
+            ),
+            Shadow(
+              offset: const Offset(1.2, 1.2),
+              blurRadius: 5.0,
+              color: Colors.yellow.shade700,
+            ),
+          ],
+        ),
+        "WIN",
+      ),
+    );
+  }
+
+  Widget _starBackground(BoxConstraints box) {
+    return AnimatedBuilder(
+      animation: _displayController,
+      builder: (ctx, child) {
+        return Opacity(
+          opacity: _displayController.value,
+          child: SizedBox(
+            height: box.H,
+            width: box.W,
+            child: AnimatedBuilder(
+              animation: _loopController,
+              child: child,
+              builder: (ctx, child) {
+                return CustomPaint(
+                  painter: WinnerStarPainter(_loopController.value),
+                  child: Container(),
+                );
+              },
+            ),
+          ),
         );
       }
+    );
+  }
+
+  Widget _foreground(BoxConstraints box) {
+    return BlurryContainer(
+      height: box.H,
+      width: box.W,
+      alignment: Alignment.center,
+      borderRadius: BorderRadius.zero,
+      blurSigmaX: 4,
+      blurSigmaY: 4,
+      child: SizedBox(
+        height: box.H,
+        width: box.W,
+        child: _winText(box),
+      ),
     );
   }
 }
